9be4c8152fd5477b1148e3a66a8ca355dfe8851d
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-03-12 21:30:06 +0700

    void
    Added input function to validate array size.

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -2,4 +2,21 @@
 using namespace std;
 
 int arr[20];
+
 int n;
+void input ()
+{
+    while (true)
+    {
+        cout << "masukkan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <=20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
